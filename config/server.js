b570c7b0c4e407f652bb5cfc2079616cbde94035
Yusmardianto <yusmardianto1@gmail.com>
2021-02-15 15:50:19 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8b68f81f57774b61603c6656e68f38fa'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a5700a1a-2cb5-45ed-80a9-99e77afa5311'
+};
